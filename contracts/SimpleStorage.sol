d95ba4218bf849662cf5fa6823a7dee9011cb570
ppalomo <ppalomov@gmail.com>
2021-05-06 15:31:27 +0200

    Updating sample contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,16 +1,33 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleStorage {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleStorage is Ownable {
 
     // Variables
     uint storedData;
+    
+    /**
+     @notice Contract constructor method.
+     */
+    constructor() {
 
-    function set(uint x) public {
+    }
+
+    function set(uint x) public onlyOwner {
         storedData = x;
     }
 
     function get() public view returns (uint) {
         return storedData;
+    }
+
+    /**
+     @notice Method used to return the contract balance.
+     @return Current contract balance.
+     */
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 }
